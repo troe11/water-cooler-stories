ce4ff866c9be2b349bd55bb168839b4ba84a62d8
Travis Roemhild <travis.roemhild@gmail.com>
2018-04-09 21:07:58 -0700

    html route

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+var path = require("path");
+
+module.exports = function(app){
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/landingpage.html"));
+      });
+
+      app.get("/signup", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/signup.html"));
+      });
+
+      app.get("/homepage", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/homepage.html"));
+      });
+
+      app.get("/add", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/addstory.html"));
+      });
+}
