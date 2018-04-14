2ab418706be49ddbd3f93f52cd8d46333313d942
kaktuxmx <kaktuxsql@gmail.com>
2018-04-14 13:52:03 -0700

    Adding DB SQL files for MYSQL WorkBench and React npm package

---
+++ models/db/insertdata.sql
@@ -0,0 +1,45 @@
+use watercoolerdb;
+
+insert into tags (tag_description)
+values ("Funny");
+insert into tags (tag_description)
+values ("Sad");
+insert into tags (tag_description)
+values ("Infuriating");
+insert into tags (tag_description)
+values ("Touching");
+insert into tags (tag_description)
+values ("Inspirational");
+insert into tags (tag_description)
+values ("Motivational");
+insert into tags (tag_description)
+values ("Scary");
+insert into tags (tag_description)
+values ("Depressing");
+insert into tags (tag_description)
+values ("Crazy");
+insert into tags (tag_description)
+values ("IFUW");
+
+insert into workdescription (work_description)
+values ("Retail");
+insert into workdescription (work_description)
+values ("Construction and Real Estate");
+insert into workdescription (work_description)
+values ("Finance and Banks");
+insert into workdescription (work_description)
+values ("Public Administration");
+insert into workdescription (work_description)
+values ("Food Services");
+insert into workdescription (work_description)
+values ("Health Care");
+insert into workdescription (work_description)
+values ("Educational Services");
+insert into workdescription (work_description)
+values ("Transportation and Warehousing");
+insert into workdescription (work_description)
+values ("IT and Software Development");
+insert into workdescription (work_description)
+values ("Hospitality");
+insert into workdescription (work_description)
+values ("Self-employed");
