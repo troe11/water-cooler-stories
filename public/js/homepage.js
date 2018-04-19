613ea49a9b36b6f79b9f4ba93cf6d4876f4b586b
Travis Roemhild <travis.roemhild@gmail.com>
2018-04-18 18:42:25 -0700

    updated homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 
     $(".jobId").on('click', function () {
+        $(".panel-body").empty();
         var category = $(this).attr('id');
-        console.log(category);
         $.get('api/stories/' + category, function(data){
             console.log(data);
             for(let i = 0; i<data.length;i++){
@@ -13,19 +13,5 @@ $(document).ready(function(){
             };
         })
     });
-
-    // getStories();
-
-    function getStories(){
-        $.get('api/stories/',function(data){
-            for(let i = 0; i<data.length;i++){
-                var x = $('<div>').text(i+1).addClass("storyDiv");
-                var y = $('<br><p>').text(data[i].vent_story)
-                x.append(y)
-                $(".panel-body").append(x);
-            }
-        })
-    }
-
-})
+});
 
